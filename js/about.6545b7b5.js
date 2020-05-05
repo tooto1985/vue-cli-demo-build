2007c481dfed692dab9a48ef4380c40928d0e675
Tommy <tooto1985@gmail.com>
2020-05-05 14:16:22 +0800

    modify

---
+++ js/about.6545b7b5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var u=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h2",[t._v("重新整理也會先透過nginx取得/index.html頁面內容")])])}],i={mounted:function(){document.title="關於頁面"}},l=i,a=e("2877"),o=Object(a["a"])(l,u,c,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=about.6545b7b5.js.map
