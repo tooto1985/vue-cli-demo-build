787a60b357aa1827d06f506a148b9c310b18bc6f
Tommy <tooto1985@gmail.com>
2020-06-03 15:50:55 +0800

    modify

---
+++ js/About.0d85934e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["About"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"container"},[n("h1",[t._v(t._s(t.$t("aboutpage")))]),n("el-button",{attrs:{type:"primary"}},[t._v("測試Eelement-ui")])],1)},s=[],u=n("2877"),l={},r=Object(u["a"])(l,a,s,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=About.0d85934e.js.map
